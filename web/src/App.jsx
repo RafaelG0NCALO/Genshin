93611827e282cacbfb1eac1c66f792ad24cf9c98
Rafael <gonalo.rafael@email.com>
2024-02-01 18:17:23 -0300

    Mensagem descritiva das alterações

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -8,34 +8,37 @@ import name from './assets/name.svg'
 import avatar1 from './assets/avatar1.svg'
 import avatar2 from './assets/avatar2.svg'
 import avatar3 from './assets/avatar3.svg'
-import perso from './assets/perso.png'
+import perso from './assets/perso.svg'
 import iconUser from './assets/iconUser.svg'
 import logo from './assets/logo.png'
 import buttonPlay from './assets/buttonPlay.svg'
 import Menu from './components/Menu';
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 const App = () => {
   return (
     <div className='w-full min-h-screen flex p-3 items-center justify-center bg-[#1E1F27]'>
       <div className="w-full bg-[url('./assets/bg.png')] max-w-7xl bg-no-repeat bg-cover h-[720px] overflow-hidden rounded-md m-auto relative">
 
+
         <div className='animate-slideUpScale absolute max-md:-right-12 right-0 top-0 z-30 flex flex-col'>
-            <img src={side} alt="" />
+            <LazyLoadImage src={side} alt="" effect="blur"/>
             <div className='absolute w-20 flex items-center flex-col max-md:right-9 right-4 max-md:top-16 top-10'>
-              <img src={iconUser} alt="" className='cursor-pointer mb-16 max-md:w-10 max-md:mb-8' />
+              <LazyLoadImage src={iconUser} alt="" effect="blur" className='cursor-pointer mb-16 max-md:w-10 max-md:mb-8' />
               <div className='flex flex-col gap-5'>
-                <img src={avatar1} alt="" className='object-contain max-md:w-9 w-12 cursor-pointer' />
-                <img src={avatar2} alt="" className='object-contain max-md:w-9 w-12 cursor-pointer' />
-                <img src={avatar3} alt="" className='object-contain max-md:w-9 w-12 cursor-pointer' />
+                <LazyLoadImage src={avatar1} alt="" effect="blur" className='object-contain max-md:w-9 w-12 cursor-pointer' />
+                <LazyLoadImage src={avatar2} alt="" effect="blur" className='object-contain max-md:w-9 w-12 cursor-pointer' />
+                <LazyLoadImage src={avatar3} alt="" effect="blur" className='object-contain max-md:w-9 w-12 cursor-pointer' />
               </div>
             </div>
         </div>
 
         <div className='flex h-1/2'>
             <header className='p-4 w-full max-md:h-14 h-24 max-md:flex justify-center'>
-              <img src={logo} alt="" className='object-contain animate-fadeIn'/>
+              <LazyLoadImage src={logo} alt="" effect="blur" className='object-contain animate-fadeIn'/>
             </header>
-            <img src={name} alt="" className='animate-slideUpScale absolute max-md:w-60 max-md:-left-4 left-0 object-contain top-6' />
+            <LazyLoadImage src={name} alt="" className='animate-slideUpScale absolute max-md:w-60 max-md:-left-4 left-0 object-contain top-6' />
             <img src={perso} alt="" className='animate-slideDown absolute object-contain max-md:scale-100 max-md:h-auto h-full -right-10 top-0 max-md:top-10 max-md:right-0' />
         </div>
 
--- web/src/components/Card.jsx
+++ web/src/components/Card.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import iconCard from '../assets/iconCard.png'
 import translate from '../assets/translate.svg'
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 const Card = ({card, translateContent}) => {
   return (
     <div className='w-full min-w-[300px] cursor-pointer first:opacity-100 opacity-65 transition-all  hover:opacity-100 max-w-[400px] rounded-3xl relative'>
         
         <div className='w-16 h-16 cursor-pointer hover:bg-gray-200 absolute right-0 rounded-full border-4 border-black bg-white flex items-center justify-center'>
-            <img src={iconCard} alt="" className='w-5' />
+            <LazyLoadImage src={iconCard} alt="" effect="blur" className='w-5' />
         </div>
            
         <div className='w-full h-full '>
-            <img src={card} alt="" className='w-full h-full object-contain' />
+            <LazyLoadImage src={card} alt="" effect="blur" className='w-full h-full object-contain' />
         </div>
         {translateContent &&
         <div className='w-12 h-24 flex items-center flex-col justify-center rounded-full absolute right-3 bottom-2 bg-black'>
             <p className='text-white mb-1'>Eng</p>
             <span className='h-3 border-[1px] bg-white block'/>
             <div className='h-9 w-9 mt-2 rounded-full bg-white flex items-center justify-center'>
-                <img src={translate} alt="" />
+                <LazyLoadImage src={translate} alt="" effect="blur" />
             </div>
         </div>
         }
